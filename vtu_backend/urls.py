2b59b0a0e9d1966dcc41098ddddc80ff030df549
Soft <salisusoftadamu@gmail.com>
2025-12-11 07:33:03 +0100

    add all missing files and apps

--- vtu_backend/urls.py
+++ vtu_backend/urls.py
@@ -3,12 +3,13 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # 1. The new Home path
+    # 1. The Home Path (Simple and clean)
     path('', views.home, name='home'),
 
-    # 2. Admin path
+    # 2. Admin Path
     path('admin/', admin.site.urls),
 
+    # 3. Your API Paths
     path('api/services/', include('services.urls')),
     path('api/auth/', include('authentication.urls')),
     path('api/transaction/', include('transaction.urls')),
